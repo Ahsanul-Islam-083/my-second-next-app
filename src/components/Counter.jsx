761ea443a53fdffed15c1710a2d7f9427e13ff03
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-04-10 00:26:12 +0600

    Refactor Counter component to manage state and update display on button click

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,16 @@
+"use client";
 
+import { useState } from "react";
 
 const Counter = () => {
-    console.log('Counter value in counter component');
-    
+    const [count, setCount] = useState(0);
+
+    console.log('Counter value in counter component', count);
     return (
         <div>
-            <h2 className='text-5xl'>Counter: 0</h2>
-            <button className='bg-blue-500 text-white px-4 py-2 rounded'>Increment</button>
+            <h2 className='text-5xl'>Counter: {count}</h2>
+            <button onClick={()=>setCount(count+1)}
+             className='bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded hover:cursor-pointer'>Increment</button>
         </div>
     );
 };
